3b511f0b3cc6d79734a83b5788fd49a7b02a789d
Wojciech Jarosz <jaroszw@gmail.com>
2021-06-02 15:39:47 +0200

    sign-in ui implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" component={HomePage} exact />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
