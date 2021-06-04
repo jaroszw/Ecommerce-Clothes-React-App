2cd5f0db93860bad37339fa1027b94b7acdde27e
Wojciech Jarosz <jaroszw@gmail.com>
2021-06-04 14:52:44 +0200

    redux-persist implemented

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+
+  document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './rootReducer';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./rootReducer";
+import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [];
 
-const store = createStore(
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
+export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middlewares),
@@ -12,4 +17,4 @@ const store = createStore(
   )
 );
 
-export default store;
+export const persistor = persistStore(store);
