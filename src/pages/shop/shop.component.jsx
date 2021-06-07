bb0febb9f5d86853956ed6e3e76b6a7861838012
Wojciech Jarosz <jaroszw@gmail.com>
2021-06-07 08:23:21 +0200

    both - container patern and HOC patern in shopPage Component before refactoring to CONTAINER

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, withRouter } from "react-router-dom";
 
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
 //Firebase utils
 import {
   firestore,
   convertCollectionSnapshotToMap,
-} from '../../firebase/firebase.utils';
+} from "../../firebase/firebase.utils";
 
 //Redux
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 //Thunk and Redux
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
 import {
   selectIsCollectionFetching,
   selectIsCollectionsLoaded,
-} from '../../redux/shop/shop.selector';
+} from "../../redux/shop/shop.selector";
 
 //Components
-import CollectionOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
+import CollectionOverviewContainer from "../../components/collections-overview/collections-overview.container";
+import CollectionPage from "../collection/collection.component";
 
-const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends Component {
@@ -32,24 +31,18 @@ class ShopPage extends Component {
 
   componentDidMount() {
     this.props.fetchCollectionsStartAsync();
-    console.log('SHOP');
+    console.log("SHOP");
   }
 
   render() {
-    const { match, isCollectionsFetching, selectIsCollectionsLoaded } =
-      this.props;
+    const { match, selectIsCollectionsLoaded } = this.props;
 
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
-          render={(props) => (
-            <CollectionOverviewWithSpinner
-              isLoading={isCollectionsFetching}
-              {...props}
-            />
-          )}
+          component={CollectionOverviewContainer}
         />
         <Route
           path={`${match.path}/:collectionId`}
@@ -70,7 +63,6 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionsFetching: selectIsCollectionFetching,
   selectIsCollectionsLoaded: selectIsCollectionsLoaded,
 });
 
